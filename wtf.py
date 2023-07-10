e926aa6b3910e554684953b29880b2894765bfa3
Alex Taylor <alex@goldenmeanie.net>
2023-07-10 11:44:51 -0400

    Adding more targeted link for contributors to acronyms repo

--- wtf.py
+++ wtf.py
@@ -65,7 +65,7 @@ def slack():
     except KeyError:
         response = """
         Entry for '{}' not found! Acronyms may be added at
-        https://github.com/department-of-veterans-affairs/acronyms
+        https://github.com/department-of-veterans-affairs/acronyms/acronyms.csv
         """.format(req['text'])
 
     return make_response(response)
